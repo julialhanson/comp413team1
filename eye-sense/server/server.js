259f987c1c7ea90e4bf320ed34480f05f9a614df
Richard Gao <richard.g2002@gmail.com>
2025-02-19 00:31:24 -0600

    WIP: Set up MongoDB database
    Going through tutorials to set up a MongoDB database. Currently have
    some version of connecting to the server and currently am building an
    API.

---
+++ eye-sense/server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import users from "./routes/user.js"
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/user", users);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
