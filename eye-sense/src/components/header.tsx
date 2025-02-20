058321e823458f34104e1b6a1137d1828fc7401e
myco <myco.le.716@gmail.com>
2025-02-19 21:04:06 -0600

    add togglable profile menu

--- eye-sense/src/components/header.tsx
+++ eye-sense/src/components/header.tsx
@@ -1,14 +1,16 @@
-// import React from "react";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import ProfileMenu from "./profile-menu";
 
 const Header = () => {
   const location = useLocation();
 
   const getLinkStyle = (path: string) => ({
     fontWeight: location.pathname === path ? "bold" : "normal",
-    textDecoration: "none",
-    padding: "10px",
+    padding: "0px 10px",
   });
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <header className="fixed top-0 flex flex-nowrap items-center justify-between w-screen h-12 p-2 bg-white">
@@ -25,11 +27,21 @@ const Header = () => {
         </Link>
       </nav>
 
-      <img
-        className="h-full rounded-full"
-        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-        alt=""
-      />
+      <div className="h-full">
+        <button
+          className="aspect-square h-full"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <img
+            className="rounded-full"
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            alt=""
+          />
+        </button>
+        {isMenuOpen && (
+          <ProfileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        )}
+      </div>
     </header>
   );
 };
---
+++ eye-sense/src/components/profile-menu.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+type ProfileMenuProps = {
+  isMenuOpen: boolean;
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const ProfileMenu = ({ isMenuOpen, setIsMenuOpen }: ProfileMenuProps) => {
+  return (
+    <>
+      <div
+        className="w-screen h-screen absolute top-0 left-0 bg-transparent"
+        onClick={() => {
+          console.log("clicked");
+          setIsMenuOpen(!isMenuOpen);
+        }}
+      ></div>
+      <div
+        id="profile-menu"
+        className="absolute right-2 mt-2 w-50 bg-white rounded-xl shadow-lg p-3 flex flex-col"
+      >
+        <Link className="hover-underline" to="/account">
+          Account
+        </Link>
+        <Link className="hover-underline" to="/surveys">
+          My Surveys
+        </Link>
+        <Link className="hover-underline" to="/survey-history">
+          Survey History
+        </Link>
+        <Link className="hover-underline" to="/settings">
+          Settings
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProfileMenu;
