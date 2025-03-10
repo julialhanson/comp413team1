bdd505bd68219ee0c7f22bf857d0240d6d97777e
Disha Baldawa <dishabaldawa@Dishas-MacBook-Air-7.local>
2025-03-09 20:52:18 -0500

    Validation for schemas

--- eye-sense/src/login-authentication/index.js
+++ eye-sense/src/login-authentication/index.js
@@ -4,16 +4,13 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import bodyParser from 'body-parser';
 
-
 dotenv.config();
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
 const app = express()
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGO_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	  })
+mongoose.connect(process.env.MONGO_URL)
 
 const userSchema = new mongoose.Schema(
 	{
@@ -22,7 +19,7 @@ const userSchema = new mongoose.Schema(
 		email: {type: String, required: true, unique: true},
 		displayName: {type: String, required: true},
 		organization: [{ type: String }],
-		role: { type: String, required: true },
+		role: { type: String, required: true},
 		organization_permissions: {
 				type: Map,
 				of: String,
@@ -34,23 +31,28 @@ const User = mongoose.model('User', userSchema)
 
 app.post('/register', async(req, res)=> {
 	try {
-		const hashedPassword = await bcrypt.hash(req.body-password, 10)
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		// TODO: if password isnt given then data and salt args err
 		const user = new User({
 			username: req.body.username,
-			password: hashedPassword
+			password: hashedPassword,
+			email: req.body.email,  
+			displayName: req.body.displayName,
+			role: req.body.role || "user"
 		})
 		const result = await user.save();
-		res.send("User registered")
-	}
-	catch(err) {
-		res.send(err)
-	}
+		res.status(201).json({ message: "User registered successfully", user: result });
+	} catch (err) {
+		res.status(500).json({
+			error: err.message
+		});
+	} 
 })
 
 app.post('/login', async(req, res) => {
-	const user = await User.findOne({username: req.body-username})
+	const user = await User.findOne({username: req.body.username})
 	if (user){
-		const isValidPassword = await bcrypt.compare(req.body-password, user-password)
+		const isValidPassword = await bcrypt.compare(req.body.password, user.password)
 		if(isValidPassword) {
 			res.send ('Logged in')
 		}
@@ -63,5 +65,4 @@ app.post('/login', async(req, res) => {
 	}
 })
 
-app.listen(3000, () => {console.log('this is port 3000')})
-
+app.listen(process.env.PORT, () => {console.log('this is port', process.env.PORT)})
