251327b2c7432776357a43870cdfbf12acc76036
dishabaldawa <baldawadisha@gmail.com>
2025-03-27 01:24:51 -0500

    JWT Token

--- eye-sense/src/login-authentication/index.js
+++ eye-sense/src/login-authentication/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 console.log("MONGO_URL:", process.env.MONGO_URL);
@@ -29,40 +30,48 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model('User', userSchema)
 
-app.post('/register', async(req, res)=> {
-	try {
-		const hashedPassword = await bcrypt.hash(req.body.password, 10)
-		// TODO: if password isnt given then data and salt args err
-		const user = new User({
-			username: req.body.username,
-			password: hashedPassword,
-			email: req.body.email,  
-			displayName: req.body.displayName,
-			role: req.body.role || "user"
-		})
-		const result = await user.save();
-		res.status(201).json({ message: "User registered successfully", user: result });
-	} catch (err) {
-		res.status(500).json({
-			error: err.message
-		});
-	} 
-})
+// app.post('/register', async(req, res)=> {
+// 	try {
+// 		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+// 		const user = new User({
+// 			username: req.body.username,
+// 			password: hashedPassword,
+// 			email: req.body.email,  
+// 			displayName: req.body.displayName,
+// 			role: req.body.role || "user"
+// 		})
+// 		const result = await user.save();
+// 		res.status(201).json({ message: "User registered successfully", user: result });
+// 	} catch (err) {
+// 		res.status(500).json({
+// 			error: err.message
+// 		});
+// 	} 
+// })
 
 app.post('/login', async(req, res) => {
-	const user = await User.findOne({username: req.body.username})
-	if (user){
-		const isValidPassword = await bcrypt.compare(req.body.password, user.password)
-		if(isValidPassword) {
-			res.send ('Logged in')
-		}
-		else {
-			res.send('Inncorect Password')
-		}
-	}
-	else {
-		res.send('Incorrect Username')
-	}
+    const user = await User.findOne({username: req.body.username})
+    if (user){
+        const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+        if(isValidPassword) {
+            const token = jwt.sign(
+                {
+                    username: user.username,
+                    role: user.role
+                }, 
+                process.env.JWT_SECRET,
+                { expiresIn: '1h' }, (err, token) => {
+                    if(err) { console.log(err) }    
+                    res.send(token)
+                });
+        }
+        else {
+            res.send('Inncorect Password')
+        }
+    }
+    else {
+        res.send('Incorrect Username')
+    }
 })
 
 app.listen(process.env.PORT, () => {console.log('this is port', process.env.PORT)})
