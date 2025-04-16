67572d49b781ee25e98d3d9747cc9122fbfc366a
myco <myco.le.716@gmail.com>
2025-04-15 23:47:58 -0500

    add refresh button on timeout modal

--- eye-sense/src/components/timeout-modal.tsx
+++ eye-sense/src/components/timeout-modal.tsx
@@ -7,10 +7,19 @@ const TimeoutModal = () => {
       <div className="w-screen h-screen absolute top-0 left-0 transparent-black-bg"></div>
       <div className="center-screen bg-white rounded-xl p-6">
         <h1 className="font-bold text-xl">Your session has expired.</h1>
-        <p className="mt-1 mb-4">Please log in again to continue.</p>
+        <p className="mt-1 mb-4">
+          Please log in again to continue or refresh the page.
+        </p>
         <Link to={`/auth`} state={{ redirectIsRegistering: false }}>
-          <button className="btn blue-btn">Login</button>
+          <button className="btn blue-btn mr-2">Login</button>
         </Link>
+
+        <button
+          className="btn blue-btn"
+          onClick={() => window.location.reload()}
+        >
+          Refresh
+        </button>
       </div>
     </>
   );
