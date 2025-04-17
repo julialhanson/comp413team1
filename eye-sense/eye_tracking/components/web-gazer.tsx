3e316ffe37b8c0ed7c4790747c694e1de31825ca
myco <myco.le.716@gmail.com>
2025-04-16 21:25:06 -0500

    display web tracker calibration in iframe

--- eye-sense/eye_tracking/components/web-gazer.tsx
+++ eye-sense/eye_tracking/components/web-gazer.tsx
@@ -1,115 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../eye-tracker.scss";
-import { loadScript } from "../utils/load-script";
-import EyeTracker from "../utils/eye-tracker";
-import { loadPyodide } from "pyodide";
+import Container from "../../src/components/container";
 
 const WebGazer = () => {
-  // const [pyodide, setPyodide] = useState<any>();
-
-  // useEffect(() => {
-  //   let webgazerInstance;
-
-  //   const init = async () => {
-  //     const webgazer = await loadScript(
-  //       "https://webgazer.cs.brown.edu/webgazer.js",
-  //       "webgazer"
-  //     );
-
-  //     webgazerInstance = webgazer;
-  //     webgazerInstance
-  //       .setGazeListener((data, timestamp) => {
-  //         // if (data) {
-  //         //   console.log("Gaze data:", data);
-  //         // }
-  //       })
-  //       .begin();
-
-  //     let pyodideInstance;
-  //     try {
-  //       pyodideInstance = await loadPyodide({
-  //         indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.5/full/",
-  //       });
-  //       // setPyodide(pyodideInstance);
-  //     } catch (error) {
-  //       console.error("Error loading Pyodide", error);
-  //     }
-
-  //     return [webgazerInstance, pyodideInstance];
-  //   };
-
-  //   init().then(([webgazerInstance, pyodideInstance]) => {
-  //     const eyeTracker = new EyeTracker(webgazerInstance, pyodideInstance);
-  //     try {
-  //       eyeTracker
-  //         .loadImageAsBase64("mountains.jpg")
-  //         .then((backgroundImageBase64) => {
-  //           console.log(
-  //             "Background image Base64 length:",
-  //             backgroundImageBase64.length
-  //           );
-
-  //           eyeTracker.loadPyodideAndPackages().then(async () => {
-  //             eyeTracker.createCalibrationDots();
-  //             eyeTracker.startTrackingPhase();
-  //             eyeTracker.beginWebgazer();
-  //           });
-  //         });
-  //     } catch (err) {
-  //       console.error("Error loading background image:", err);
-  //     }
-  //   });
-
-  //   return () => {
-  //     if (webgazerInstance) {
-  //       webgazerInstance.end(); // Clean up on unmount
-  //     }
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   // Add eye tracker libraries
-  //   const script = document.createElement("script");
-
-  //   // script.src = "https://webgazer.cs.brown.edu/webgazer.js";
-  //   // document.body.appendChild(script);
-
-  //   script.src = "https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js";
-  //   document.body.appendChild(script);
-
-  //   try {
-  //     loadImageAsBase64("mountains.jpg").then((backgroundImageBase64) => {
-  //       console.log(
-  //         "Background image Base64 length:",
-  //         backgroundImageBase64.length
-  //       );
-
-  //       createCalibrationDots();
-  //       // Start WebGazer so that the webcam is active (HTTPS/localhost required)
-  //       webgazer.begin();
-  //       webgazer.showVideo(true).showPredictionPoints(true);
-  //     });
-  //   } catch (err) {
-  //     console.error("Error loading background image:", err);
-  //   }
-  // }, []);
-
   return (
-    // <div className="web-gazer-container">
-    //   <div id="instructions">Click all 9 dots to calibrate...</div>
-    //   <div id="gazeDot"></div>
-    //   <div id="imagePhase">
-    //     <img id="trackingImage" src="mountains.jpg" alt="Tracking Image" />
-    //   </div>
-    //   <img id="heatmapImg" alt="Heatmap" />
-    // </div>
-    <iframe
-      src="/wg.html"
-      title="WebGazer Heatmap"
-      width="100%"
-      height="1000px"
-      style={{ border: "none" }}
-    />
+    <Container>
+      <iframe
+        src="/web-gazer.html"
+        title="WebGazer Heatmap"
+        width="100%"
+        height="700px"
+        style={{ border: "none" }}
+      />
+    </Container>
   );
 };
 
