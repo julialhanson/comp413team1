5816bdaeb9144edd71a77b2091a7b8cd7c6a28b9
myco <myco.le.716@gmail.com>
2025-04-16 23:25:28 -0500

    modularize modal

---
+++ eye-sense/src/components/modal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TimeoutModal = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="w-screen h-screen absolute top-0 left-0 transparent-black-bg"></div>
+      <div className="center-screen bg-white rounded-xl p-6">{children}</div>
+    </>
+  );
+};
+
+export default TimeoutModal;
--- eye-sense/src/components/timeout-modal.tsx
+++ eye-sense/src/components/timeout-modal.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Modal from "./modal";
 
 const TimeoutModal = () => {
   return (
-    <>
-      <div className="w-screen h-screen absolute top-0 left-0 transparent-black-bg"></div>
-      <div className="center-screen bg-white rounded-xl p-6">
-        <h1 className="font-bold text-xl">Your session has expired.</h1>
-        <p className="mt-1 mb-4">
-          Please log in again to continue or refresh the page.
-        </p>
+    <Modal>
+      <h1 className="font-bold text-xl">Your session has expired.</h1>
+      <p className="mt-1 mb-4">
+        Please log in again to continue or refresh the page.
+      </p>
 
-        <center>
-          <Link to={`/auth`} state={{ redirectIsRegistering: false }}>
-            <button className="btn blue-btn mr-2">Login</button>
-          </Link>
+      <center>
+        <Link to={`/auth`} state={{ redirectIsRegistering: false }}>
+          <button className="btn blue-btn mr-2">Login</button>
+        </Link>
 
-          <button
-            className="btn blue-btn"
-            onClick={() => window.location.reload()}
-          >
-            Refresh
-          </button>
-        </center>
-      </div>
-    </>
+        <button
+          className="btn blue-btn"
+          onClick={() => window.location.reload()}
+        >
+          Refresh
+        </button>
+      </center>
+    </Modal>
   );
 };
 
