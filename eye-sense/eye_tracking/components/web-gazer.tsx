f1b150b85266d79100dae9de0348ee4887e621f1
myco <myco.le.716@gmail.com>
2025-04-18 03:24:40 -0500

    open web gazer on image click (but image not dynamic yet)

--- eye-sense/eye_tracking/components/web-gazer.tsx
+++ eye-sense/eye_tracking/components/web-gazer.tsx
@@ -1,16 +1,20 @@
+import Modal from "../../src/components/modal";
+
 const WebGazer = () => {
   return (
-    <div className="max-w-4xl ml-auto mr-auto p-5">
-      <div className="m-5 rounded-2xl bg-white p-5">
-        <iframe
-          src="/web-gazer.html"
-          title="WebGazer Heatmap"
-          width="100%"
-          height="700px"
-          style={{ border: "none" }}
-        />
-      </div>
-    </div>
+    // <div className="max-w-4xl ml-auto mr-auto p-5">
+    //   <div className="m-5 rounded-2xl bg-white p-5">
+    // </div>
+    // </div>
+    <Modal>
+      <iframe
+        src="/web-gazer.html"
+        title="WebGazer Heatmap"
+        width="900px"
+        height="700px"
+        style={{ border: "none" }}
+      />
+    </Modal>
   );
 };
 
--- eye-sense/src/components/question-display.tsx
+++ eye-sense/src/components/question-display.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Question } from "../types";
-import { retrieveImageFromGCP } from "../controllers/gcp-controller";
+import WebGazer from "../../eye_tracking/components/web-gazer";
 
 type QuestionDisplayProps = {
   question: Question;
@@ -29,6 +29,8 @@ const QuestionDisplay = ({
 
   const isResponseDisplay = selectOption && deselectOption ? false : true;
 
+  const [webGazerIsOpen, setWebGazerIsOpen] = useState<boolean>(false);
+
   const getOptionType = (type: string) => {
     switch (type) {
       case "checkboxes":
@@ -38,89 +40,112 @@ const QuestionDisplay = ({
     }
   };
 
-  return (
-    <div className="bg-white rounded-xl mb-3 p-4" key={index}>
-      <div className="flex items-start">
-        <h1 className="font-bold">{index + 1}</h1>
-        <p className="mx-3 w-full mb-3">{question.question}</p>
-      </div>
+  const openWebGazer = () => {
+    setWebGazerIsOpen(true);
+  };
 
-      {/* DISPLAY QUESTION OPTIONS */}
-      <div className="grid grid-cols-2">
-        <div>
-          {question.type !== "dropdown" ? (
-            <>
-              {question.choices.map((choice) => (
-                <div className="mb-2 w-fit">
-                  <label key={choice._id}>
-                    <input
-                      className="mr-2"
-                      name={"question-" + index}
-                      type={getOptionType(question.type)}
-                      disabled={isResponseDisplay}
-                      checked={
-                        choice._id
-                          ? selectedOptionIds?.includes(choice._id)
-                          : false
-                      }
-                      value={choice.text}
-                      onChange={
-                        selectOption && deselectOption
-                          ? (e) => {
-                              if (e.target.checked)
-                                selectOption(index, choice._id);
-                              else deselectOption(index, choice._id);
-                            }
-                          : undefined
-                      }
-                    />
-                    {choice.text}
-                  </label>
-                </div>
-              ))}
-            </>
-          ) : (
-            <select
-              className="border rounded-md p-2"
-              onChange={
-                selectOption
-                  ? (e) => selectOption(index, e.target.value)
-                  : undefined
-              }
-              disabled={isResponseDisplay}
-            >
-              <option value="" selected hidden>
-                Select an option...
-              </option>
-              {question.choices.map((choice) => (
-                <option
-                  key={choice._id}
-                  value={choice._id}
-                  selected={
-                    choice._id ? selectedOptionIds?.includes(choice._id) : false
-                  }
-                >
-                  {choice.text}
-                </option>
-              ))}
-            </select>
-          )}
+  return (
+    <>
+      <div className="bg-white rounded-xl mb-3 p-4" key={index}>
+        <div className="flex items-start">
+          <h1 className="font-bold">{index + 1}</h1>
+          <p className="mx-3 w-full mb-3">{question.question}</p>
         </div>
 
-        {question.image && (
-          <div className="m-2">
-            <img
-              src={
-                question.image instanceof File
-                  ? URL.createObjectURL(question.image)
-                  : question.image
-              }
-              alt=""
-            />
+        {/* DISPLAY QUESTION OPTIONS */}
+        <div className="grid grid-cols-2">
+          <div>
+            {question.type !== "dropdown" ? (
+              <>
+                {question.choices.map((choice) => (
+                  <div className="mb-2 w-fit">
+                    <label key={choice._id}>
+                      <input
+                        className="mr-2"
+                        name={"question-" + index}
+                        type={getOptionType(question.type)}
+                        disabled={isResponseDisplay}
+                        checked={
+                          choice._id
+                            ? selectedOptionIds?.includes(choice._id)
+                            : false
+                        }
+                        value={choice.text}
+                        onChange={
+                          selectOption && deselectOption
+                            ? (e) => {
+                                if (e.target.checked)
+                                  selectOption(index, choice._id);
+                                else deselectOption(index, choice._id);
+                              }
+                            : undefined
+                        }
+                      />
+                      {choice.text}
+                    </label>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <select
+                className="border rounded-md p-2"
+                onChange={
+                  selectOption
+                    ? (e) => selectOption(index, e.target.value)
+                    : undefined
+                }
+                disabled={isResponseDisplay}
+              >
+                <option value="" selected hidden>
+                  Select an option...
+                </option>
+                {question.choices.map((choice) => (
+                  <option
+                    key={choice._id}
+                    value={choice._id}
+                    selected={
+                      choice._id
+                        ? selectedOptionIds?.includes(choice._id)
+                        : false
+                    }
+                  >
+                    {choice.text}
+                  </option>
+                ))}
+              </select>
+            )}
           </div>
-        )}
+
+          {question.image && (
+            <div className="relative m-2">
+              <img
+                src={
+                  question.image instanceof File
+                    ? URL.createObjectURL(question.image)
+                    : question.image
+                }
+                alt=""
+              />
+
+              {question.is_tracking && (
+                <>
+                  <div className="size-full transparent-black-bg center-screen"></div>
+
+                  <p
+                    onClick={() => openWebGazer()}
+                    className="center-screen italic grey-btn btn w-max"
+                  >
+                    Start eye tracking
+                  </p>
+                </>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+
+      {webGazerIsOpen && <WebGazer />}
+    </>
   );
 };
 
