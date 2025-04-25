09130f55ee2a5ba1f5c3e2e05f8fd24513412a3d
myco <myco.le.716@gmail.com>
2025-04-25 04:19:34 -0500

    center text in timeout modal

--- eye-sense/src/components/timeout-modal.tsx
+++ eye-sense/src/components/timeout-modal.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Modal from "./modal";
 
 const TimeoutModal = () => {
   return (
     <Modal>
-      <h1 className="font-bold text-xl">Your session has expired.</h1>
-      <p className="mt-1 mb-4">
-        Please log in again to continue or refresh the page.
-      </p>
-
       <center>
+        <h1 className="font-bold text-xl">Your session has expired.</h1>
+        <p className="mt-1 mb-4">
+          Please log in again to continue or refresh the page.
+        </p>
+
         <Link to={`/auth`} state={{ redirectIsRegistering: false }}>
           <button className="btn blue-btn mr-2">Login</button>
         </Link>
