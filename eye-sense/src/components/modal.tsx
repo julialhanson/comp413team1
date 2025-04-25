96db65d1bd4d1ab2397bfe028463d7d398072ba5
myco <myco.le.716@gmail.com>
2025-04-25 04:21:15 -0500

    pull modal backdrop over header

--- eye-sense/src/components/modal.tsx
+++ eye-sense/src/components/modal.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TimeoutModal = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="w-screen h-screen absolute top-0 left-0 transparent-black-bg z-20"></div>
+      <div className="w-screen h-screen absolute top-0 left-0 transparent-black-bg z-30"></div>
       <div className="center-screen bg-white rounded-xl p-6 relative z-30">
         {children}
       </div>
